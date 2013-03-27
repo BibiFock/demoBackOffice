b8bb98fce3b399537a5a2e96262378408871ae2e
julien <julien@julien-laptop.(none)>
2013-03-27 14:33:15 +0100

    rename Accesstype

---
+++ src/DemoBackOffice/Model/Entity/AccessType.php
@@ -0,0 +1,33 @@
+<?php
+namespace DemoBackOffice\Model\Entity{
+
+	class AccessType{
+
+		public static $FORBIDDEN = 1;
+		public static $READONLY = 2;
+		public static $EDIT = 3;
+		public $type;
+
+		public static function getStaticValues($var){
+			if(isset(self::$$var)) return self::$$var;
+			return null;
+		}
+
+		public function __construct($type){
+			$this->type = $type;
+		}
+
+		public function canRead(){
+			return ($this->type >= self::$READONLY);
+		}
+
+		public function canEdit(){
+			return ($this->type >= self::$EDIT);
+		}
+
+		public function setAdminMode(){
+			$this->type = self::$EDIT;
+		}
+	}
+
+}
